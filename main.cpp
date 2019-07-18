54330a2b5e52d247b023852d08e5928a84d378f9
astisk <pobierac@gmail.com>
2019-07-18 18:30:55 +0200

    Added class Matrix with basic mathematical operations

--- main.cpp
+++ main.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 #include "Vector3.cpp"
 #include "Vector2.cpp"
+#include "Matrix.cpp"
 
 using namespace std;
 
 int main()
 {
+    int m=3;
+    int n=3;
+    std::vector<int> arraj;
+
+    for(int i=0; i<m; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                arraj.push_back(i*n+j+5);
+            }
+        }
+    arraj[8]=1;
+
+    Matrix<int> a(arraj, m, n);
+
+    int det = Determinant<int>(a);
+    cout<<"Determinant="<<det<<endl;
     return 0;
 }
